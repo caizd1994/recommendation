397639e268f02c58a5342dae0f72538e3f5b5383
zhidongcai <zhidongcai@tencent.com>
2019-04-11 15:41:46 +0800

    deepFM

---
+++ deepfm/deepFM_Model.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from common_utils.utils import get_optimizer
+
+class deepFM(object):
+    def __init__(self):
+        pass
+
+    def build_FM_layer(self,features,parms):
+        #line_part
+        bias = tf.get_variable("fm_bias", shape=[1], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+
+        line_emb = tf.feature_column.input_layer(features,parms['first_feature_columns'])
+        line_logits = tf.reduce_sum(line_emb,axis=1,keepdims=True)
+
+        fm_emb = tf.feature_column.input_layer(features,parms['second_feature_columns'])
+        #sum_squre
+        sum_squre = tf.reduce_sum(fm_emb,axis=1,keepdims=True)
+        sum_squre = tf.square(sum_squre)
+        #squre_sum
+        squre_sum = tf.square(fm_emb)
+        squre_sum = tf.reduce_sum(squre_sum,axis=1,keepdims=True)
+
+        fm_logits = 0.5*(sum_squre - squre_sum) + line_logits + bias
+        return fm_logits
+
+    def build_deep_layer(self,features,parms):
+        units = parms['hidden_units']
+        dim = units[-1]
+        last_w = tf.get_variable("final_w", shape=[dim,1],initializer=tf.truncated_normal_initializer(stddev=0.01))
+        last_b = tf.get_variable("bias_last", shape=[1], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+
+        deep_net = tf.feature_column.input_layer(features,parms['second_feature_columns'])
+
+        for layer in units:
+            deep_net = tf.layers.dense(deep_net,layer,activation=tf.nn.relu,kernel_initializer=tf.truncated_normal_initializer())
+            if parms["is_bn"] == "true":
+                deep_net = tf.layers.batch_normalization(deep_net)
+            if parms["is_dropout"] == "true":
+                deep_net = tf.layers.dropout(deep_net,rate=parms["dropout_rate"])
+        deep_net = tf.matmul(deep_net , last_w) + last_b
+        return deep_net
+
+    def train_op(self,features,labels,params):
+        labels = tf.cast(tf.reshape(labels, [-1, 1]), dtype=tf.float32)
+
+        FM_logits = self.build_FM_layer(features,params)
+        deep_logits = self.build_deep_layer(features,params)
+
+        logits = FM_logits+deep_logits
+        predicts = tf.nn.sigmoid(logits)
+        loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits,labels=labels)
+        loss = tf.reduce_mean(loss)
+
+        if(params['is_decy'] == "true"):
+            learn_rate = tf.train.exponential_decay(params['learning_rate'],tf.train.get_global_step(),decay_steps=params['decay_steps'],
+                                                    decay_rate=params['decay_rate'])
+        else:
+            learn_rate = params['decay_steps']
+
+        optimize = get_optimizer(params["optimizer"],learning_rate=learn_rate,global_step=tf.train.get_global_step())\
+            .minimize(loss, global_step=tf.train.get_global_step())
+
+        eval_metric_ops = {
+            "auc":tf.metrics.auc(labels=labels,predictions=predicts)
+        }
+        return tf.estimator.EstimatorSpec(
+            mode=tf.estimator.ModeKeys.TRAIN,
+            predictions=predicts,
+            loss=loss,
+            eval_metric_ops=eval_metric_ops,
+            train_op=optimize
+        )
+
+
+
---
+++ deepfm/model.py
@@ -0,0 +1,91 @@
+from deepfm.deepFM_Model import deepFM
+from common_utils.utils import *
+from deepfm.config import init_model_args
+
+feature_list = ["uid", "city", "item_id", "author_id", "item_city"]
+bucket_size = [73974, 396, 4122689, 850308, 461]
+# feature_list = ["city", "item_id", "author_id", "item_city"]
+# bucket_size = [396, 4122689, 850308, 461]
+
+
+class RecommendModelHandler(object):
+    def __init__(self, train_dataset_path, val_dataset_path, save_model_dir,
+                 num_epochs, batch_size, embedding_size, learning_rate,
+                 optimizer, task,is_decy,decay_steps,decay_rate,is_bn,is_dropout,dropout_rate,hidden_units,
+                 num_threads=1):
+        self._num_threads = num_threads
+        self._num_epochs = num_epochs
+        self._batch_size = batch_size
+        self._embedding_size = embedding_size
+        self._learning_rate = learning_rate
+        self._optimizer = optimizer
+        self._train_dataset_path = train_dataset_path
+        self._val_dataset_path = val_dataset_path
+        self._save_model_dir = save_model_dir
+        self._task = task
+        self.is_decy = is_decy
+        self.decay_steps = decay_steps
+        self.decay_rate = decay_rate
+        self.is_bn = is_bn
+        self.is_dropout = is_dropout
+        self.dropout_rate = dropout_rate
+        self.hidden_units = hidden_units
+
+    def build_feature(self):
+        first_feature_columns = []
+        second_feature_columns = []
+        for feat, size in zip(feature_list, bucket_size):
+            cate_id = tf.feature_column.categorical_column_with_identity(feat, size)
+            first_feature_columns.append(tf.feature_column.embedding_column(cate_id, dimension=1))
+            second_feature_columns.append(tf.feature_column.embedding_column(cate_id, dimension=self._embedding_size))
+        print("Number of feature column features:%d" % (len(first_feature_columns)))
+        return first_feature_columns, second_feature_columns
+
+    def build_model(self):
+        """ build recommend model framework"""
+        first_feature_columns, second_feature_columns = self.build_feature()
+
+        config = tf.estimator.RunConfig().replace(
+            session_config=tf.ConfigProto(device_count={'CPU': self._num_threads}),
+            log_step_count_steps=20)
+        params = {
+            'learning_rate': self._learning_rate,
+            'optimizer': self._optimizer,
+            'first_feature_columns': first_feature_columns,
+            'second_feature_columns': second_feature_columns,
+            'is_decy': self.is_decy,
+            'decay_steps': self.decay_steps,
+            'decay_rate': self.decay_rate,
+            'is_bn': self.is_bn,
+            'is_dropout': self.is_dropout,
+            'dropout_rate': self.dropout_rate,
+            "hidden_units":self.hidden_units
+        }
+        # model_fn = deepFM_.train_op()
+        deepFM_ = deepFM()
+
+        model = tf.estimator.Estimator(
+            model_fn=deepFM_.train_op,
+            model_dir=self._save_model_dir,
+            params=params,
+            config=config)
+        return model
+
+    def train(self):
+        """
+        Train model
+        """
+        parms = init_model_args()
+        model = self.build_model()
+        train_spec = tf.estimator\
+            .TrainSpec(input_fn=lambda: input_fn(filenames=parms.training_path
+                                                 ,num_epochs=parms.num_epochs
+                                                 ,batch_size=parms.batch_size
+                                                 ,features_dict=feature_list))
+        val_spec = tf.estimator.EvalSpec(input_fn=lambda: input_fn(filenames=parms.training_path
+                                                                   ,num_epochs=parms.num_epochs
+                                                                   ,batch_size=parms.batch_size*50
+                                                                   ,features_dict=feature_list), steps=1)
+        tf.estimator.train_and_evaluate(model, train_spec, val_spec)
+        # model.evaluate(input_fn=lambda: self.input_fn(data_mode='train'))
+        # pred = model.predict(input_fn=lambda: self.input_fn(data_mode='val'))
